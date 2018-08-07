ced6d3cdb9345fb6cd25e17e5f60a824fecae5c8
Brendan regan <brendanregan100@gmail.com>
2018-08-07 18:11:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 document.getElementById("main").remove();
 
 /* Create your new element here and assign it to newHeader */
-var element = document.createElement("h1");
-document.body.appendChild(element);
-element.id = "victory";
-element.innerText = "Brendan is the champion";
+var newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerText = "Brendan is the champion";
 
