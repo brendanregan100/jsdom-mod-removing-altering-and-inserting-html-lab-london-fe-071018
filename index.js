c0b273da7a8b932b9bd66100e10041e0115f2859
Brendan regan <brendanregan100@gmail.com>
2018-08-07 18:09:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /* Enter the code to remove the main node element under this comment */
+document.getElementById("main").remove();
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+var element = document.createElement("h1");
+document.body.appendChild(element);
+element.id = "victory".toString;
+element.innerHTML = "Brendan is the champion";
+
